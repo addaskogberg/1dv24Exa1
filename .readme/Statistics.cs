d862fabe15fbbd2b8e5e920c01a6612a9ad35584
addaskogberg <adda.skogberg@gmail.com>
2018-04-03 12:41:53 +0200

    class statistics

---
+++ .readme/Statistics.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace as224wq_examination_1
+{
+      static class Statistics
+    {
+
+      static int Maximum(int[] source)
+      {
+        int max;
+
+        return max;
+      }
+
+
+
+    }
+}
